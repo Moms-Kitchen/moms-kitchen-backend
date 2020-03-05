b235b1abef9d02d32df9188fcdc0daed2c701851
Miguel Rojas <miromar94@hotmail.com>
2020-03-05 16:53:47 -0500

    Archivos bases back-end
    Co-authored-by: Pedro Mayorga <pedrojmayorga@gmail.com>
    Co-authored-by: Daniel Cifuentes <danixbox1313@gmail.com>
    Co-authored-by: Nicolas Cardenas <nicocrash97@hotmail.com>

---
+++ src/test/java/com/proyect/momskitchen/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.momskitchen.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
