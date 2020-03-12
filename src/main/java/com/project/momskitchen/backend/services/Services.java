c15f49fe8bde507e946fc2a8a4e0d5a2a6890af6
Miguel Rojas <miromar94@hotmail.com>
2020-03-12 14:22:13 -0500

    Interface Services

---
+++ src/main/java/com/project/momskitchen/backend/services/Services.java
@@ -0,0 +1,9 @@
+package com.project.momskitchen.backend.services;
+
+import java.util.ArrayList;
+
+public interface Services {
+
+    public ArrayList<Object> allElements();
+    public Object getElmentObject(Object obj) throws Exception;
+}
