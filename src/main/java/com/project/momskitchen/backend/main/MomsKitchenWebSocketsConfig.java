d6eecb17edc30f1df1a3263a24f8caccc666b5b8
Miguel Rojas <miguel.rojas-ma@mail.escuelaing.edu.co>
2020-04-29 19:18:14 -0500

    Configuration websocket

--- src/main/java/com/project/momskitchen/backend/main/MomsKitchenWebSocketsConfig.java
+++ src/main/java/com/project/momskitchen/backend/main/MomsKitchenWebSocketsConfig.java
@@ -2,13 +2,13 @@ package com.project.momskitchen.backend.main;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurationSupport;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class MomsKitchenWebSocketsConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class MomsKitchenWebSocketsConfig extends WebSocketMessageBrokerConfigurationSupport {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -17,7 +17,7 @@ public class MomsKitchenWebSocketsConfig extends AbstractWebSocketMessageBrokerC
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
     }
 
 }
